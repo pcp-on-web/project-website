952fdea57671ce45c46072723735b86694711f25
triechert <thomas.riechert@htwk-leipzig.de>
2018-09-26 15:05:33 +0200

    new blog entries

--- blog.php
+++ blog.php
@@ -50,11 +50,11 @@
 <br class="blogline"/>
 <?php echo section_end(); ?> 
 
-<?php echo section_begin("Zweites  Data for History Meeting, Lyon, 24-25 Mai 2018","2nd Data for History meeting, Lyon, 24-25 May 2018",$i++,true,"26.09.2018 - Thomas Riechert"); ?> 
+<?php echo section_begin("Zweites  Data for History Meeting, Lyon, 24-25 Mai 2018","2nd Data for History meeting, Lyon, 24-25 May 2018",$i++,true,"26.05.2018 - Thomas Riechert"); ?> 
  
 <p class="de">Université Jean-Moulin Lyon 3<br/>Organised by the Pôle histoire numérique (Digital history department) of the LARHRA laboratory.<!--more--></p>
 <p class="en">Université Jean-Moulin Lyon 3<br/>Organised by the Pôle histoire numérique (Digital history department) of the LARHRA laboratory.<!--more--></p>
-<p><p><a href="http://dataforhistory.org/2nd-data-for-history" target="_blank">http://dataforhistory.org/2nd-data-for-history</a></p></p>
+<p><a href="http://dataforhistory.org/2nd-data-for-history" target="_blank">http://dataforhistory.org/2nd-data-for-history</a></p>
 <br class="blogline"/>
 <?php echo section_end(); ?>
 
