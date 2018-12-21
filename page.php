59f670bd84052d8fc6c15d9459c48b10ef11c263
triechert <thomas.riechert@htwk-leipzig.de>
2018-12-21 14:26:27 +0100

    change to single page files, some more content in cfp

--- page.php
+++ page.php
@@ -1,91 +1,13 @@
-<?php if (strpos("#RODBH2019#Partner",$_GET["goto"])===false) {header('Location: /'); die(); } ?>
-
-<?php include("parts/header.php"); ?> 
-
-
-
-<?php // Start RODBH2019
-if ($_GET["goto"]=="RODBH2019") {
-?>
-
-    <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="intro-text">
-                        <span class="de name">Doktorandensymposium: Research on Online Databases in History (RODBH 2019), 4./5. April 2019, Leipzig</span>
-                        <span class="en name">Doctoral Symposium on Research on Online Databases in History (RODBH 2019), April 4th-5th 2019, Leipzig</span>
-
-                        <hr class="star-light">
-                        <span class="de skills">Aufruf zur Einreichung von Beiträgen: 20.12.2018, Deadline: 15.02.2019, Benachrichtigung über die Annahme: 01.03.2019</span>
-                        <span class="en skills">CfP will be published December, 20th 2018, Workshop Paper deadline: February, 15th 2019, Final notification to authors: March, 1st 2019</span>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <!-- Partner Section -->
-    <?php echo section_begin("Call for Papers","Call for Papers",0); ?> 
-
-    <!-- Beginn Partner Content -->
-<p>The Doctoral Symposium on Research on Online Databases in History is intended to bring together Ph.D. students within fields of Humanities, Computer Science and Digital Humanities to discuss their research in an international forum. More precisely, the Doctoral Symposium will provide students within:
-<ul>
-<li>Presenting their research work in a scientific environment;</li>
-<li>Receiving feedback and suggestions from peers and experienced faculty;</li>
-<li>Gaining an overview of the breadth and depth of data for history;</li>
-<li>Obtain insight into directions for research on online databases in history taken by other doctoral candidates;</li>
-<li>Discuss concerns about research and methodologies, and other issues;</li>
-<li>A Network with peers and future colleagues</li>
-</ul>
-</p><p>
-The Doctoral Symposium is hosted by Leipzig University of Applied Science, Faculty of Informatics and Media (HTWK) as part of the Research project <a href="https://pcp-on-web.htwk-leipzig.de" target="_blank">Early Modern Professorial Career Patterns - Methodological research on online databases of academic history</a>, in cooperation with the European Consortium on <a href="http://dataforhistory.org/" target="_blank">Data for History</a>. A general meeting of the DfH Consortium is part of the workshop and includes the application of the OntoMe, a platform to design vocabularies using ontologies contributed by Ph.D. students.
-</p>
-
-
-<h3>Submission, Eligibility and Review Criteria</h3>
-
-<p>
-The paper submission to the doctoral symposium is open only to Doctoral or Ph.D. Students. Accepted paper at the Doctoral Symposium must be presented by the student, for which accommodation and participation are funded by the German Science Foundation (DFG). 
-</p><p>
-Submissions will be judged mainly on relevance, originality, technical quality and clarity. Papers must be written in English and be between 4 and 8 pages in length, following the LNI (Lecture Notes in Informatics) templates in Word or LaTeX provided by Gesellschaft für Informatik e.V. 
-</p><p>
-The contributions may include a vocabulary of the applied research vocabulary under Creative Commons Attribution 4.0 International Public License ("Public License"), if available in RDF format. All papers should be submitted using <a href="https://easychair.org/conferences/?conf=rodbh2019" target="_blank">EasyChair</a>.
-</p>
-
-<h3>Important Dates</h3>
-
-<p><ul><li>Paper Submission Deadline: February 15, 2019</li>
-<li>Notification of acceptance: March 1st, 2019</li>
-<li>Doctoral Symposium: April 4th to April 5th, 2019</li>
-</ul>
-</p>
-
-<h3>PC-Chair</h3>
-<p><ul><li>Thomas Riechert, Leipzig University of Applied Sciences, Germany</li>
-<li>Francesco Beretta (CNRS, LARHRA, France)</li>
-<li>George Bruseker (FORTH-ICS, CCI, Greece)</li>
-</ul></p>
-
-<h3>Organizing Committee</h3>
-<p><ul><li>Edgard Marx, Leipzig University of Applied Sciences, Germany</li>
-<li>Jennifer Blanke, Herzog-August Library, Wolfenbüttel, Germany</li>
-</ul>
-</p>
-
-
-
-
-
-
-
-    <!-- End Partner Content -->
-    <?php echo section_end(); ?> 
-
-
 <?php
-} // End RODBH2019
+$pages = array("RODBH2019","xxx");
+
+if (!@($goto=$_GET["goto"])) $goto="goto_is_empty"; 
+if (in_array($goto, $pages)) {
+	include("parts/header.php"); 
+	include("pages/".$goto.".php");
+	include("parts/footer.php");
+} else {
+	header('Location: /');
+}
 ?>
 
-<?php include("parts/footer.php"); ?> 
