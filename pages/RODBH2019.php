8b309c6d34c59462a76f8752813a36f4d2849357
triechert <thomas.riechert@htwk-leipzig.de>
2019-02-19 00:05:48 +0100

    information pdf added

--- pages/RODBH2019.php
+++ pages/RODBH2019.php
@@ -112,9 +112,11 @@ Gustav-Freytag-Str. 42A, Zi. 507, 04277 Leipzig, Germany<br/></li>
 
 <p><ul>
 <li><a href="pages/RODBH_2019_CfP.pdf" target="_blank">Call for Papers</a></li>
+<li><a href="pages/RODBH_2019_Organisation.pdf" target="_blank">Information</a></li>
 <li><a href="pages/RODBH_2019_Poster.pdf" target="_blank">Poster</a></li>
 </ul>
 </p>
+
 
 
     <!-- End Partner Content -->
