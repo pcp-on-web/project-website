1c65140876e4b97a72320c8a2c0373ac7ca619fe
Thomas Riechert <triechert@users.noreply.github.com>
2019-09-01 07:30:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
                 <span class="de">Aktuell:
                   <a href="HeloiseAtelierIX/">
 IX Workshop Héloïse – European Network on Digital Academic History
-11. bis 13. November 2019, Leipzig
+11. bis 13. November 2019, Leipzig (Einreichungen bis 9. September)
 </a>
                 </span>
                 <span class="en">News:
                   <a href="HeloiseAtelierIX/">
 IX Workshop Héloïse – European Network on Digital Academic History
-11th to 13th November 2019, Leipzig, Germany
+11th to 13th November 2019, Leipzig, Germany (submission deadline Sept. 9th)
 </a>
                 </span>
                 <span onclick="jQuery('#news').slideUp()">&#10006;</span>
