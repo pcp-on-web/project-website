397dd39d8453cd58b0e3814521d03338f97bb835
Thomas Riechert <thomas.riechert@htwk-leipzig.de>
2019-11-10 08:51:13 +0100

    abstracts added

--- HeloiseAtelierIX/index.js
+++ HeloiseAtelierIX/index.js
@@ -12,13 +12,22 @@ function mobile() {
 	}
 
 }
+
+function show_abstract(obj) {
+
+	if (obj.parentNode.nextElementSibling.style.display=='block') {
+		obj.parentNode.nextElementSibling.style.display='';
+		obj.innerHTML="show abstract"
+	}
+	else {
+		obj.parentNode.nextElementSibling.style.display='block';
+		obj.innerHTML="hide abstract"
+	}
+}
+
 document.addEventListener("DOMContentLoaded", mobile);
 window.addEventListener('resize', mobile);
 window.addEventListener('orientationchange', mobile);
 
-var more = document.querySelector('.abstract');
 
 
-more.onclick = function() {
-	alert("more");
-}
