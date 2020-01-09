bd17d59548beea6fd3d60e2c55c9a18bf2b7dd98
Thomas Riechert <thomas.riechert@htwk-leipzig.de>
2020-01-09 17:38:55 +0100

    RODBH publication added to pages

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php
-$pages = array("RODBH2019","RODBH2019-Registration","RODBH2019-Agenda","RODBH2019-AcceptedPapers");
+$pages = array("RODBH2019","RODBH2019-Publication","RODBH2019-Agenda","RODBH2019-AcceptedPapers");
 
 if (!@($goto=$_GET["goto"])) $goto="goto_is_empty"; 
 if (in_array($goto, $pages)) {
