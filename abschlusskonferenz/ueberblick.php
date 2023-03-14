d41a20a7b277baea618f1de9aa0cc64dfbbb1d44
T. Riechert <thomas.riechert@htwk-leipzig.de>
2023-03-14 18:56:37 +0100

    ueberblick

--- abschlusskonferenz/ueberblick.php
+++ abschlusskonferenz/ueberblick.php
@@ -4,12 +4,18 @@
 			<h3>Thema</h3>
 
 			<div class="text">
-				<p>Das DFG-Projekt „Professorale Karrieremuster der Frühen Neuzeit: Entwicklung einer wissenschaftlichen Methode zur Forschung auf online verfügbaren und verteilten Forschungsdatenbanken der Universitätsge­schichte“, durchgeführt von der Herzog August Bibliothek Wolfenbüttel in Kooperation mit der Hochschule für Technik, Wirtschaft und Kultur in Leipzig, lädt zur Abschlusskonferenz ein.
+				<p>Das DFG-Projekt „Professorale Karrieremuster der Frühen Neuzeit: Entwicklung einer wissenschaftlichen Methode zur Forschung auf online verfügbaren und verteilten Forschungsdatenbanken der Universitätsge­schichte“ (2016-2023), durchgeführt von der Herzog August Bibliothek Wolfenbüttel in Kooperation mit der Hochschule für Technik, Wirtschaft und Kultur in Leipzig, lädt zur Abschlusskonferenz ein.
 				</p>
-				<p>Leitfrage des Projekts ist die nach den Bedingungen für den beruflichen Erfolg von Professoren im frühneu­zeitlichen Universitätssystem. Dabei war es das Anliegen zu erproben wie mögliche gelehrte Karrieremuster mit den Methoden der Digital Humanities sichtbar gemacht werden können.
+				<p>Leitfrage des Projekts war die nach den Bedingungen für den beruflichen Erfolg von Professoren im frühneu­zeitlichen Universitätssystem. Dabei war es das Anliegen zu erproben wie mögliche gelehrte Karrieremuster mit den Methoden der Digital Humanities sichtbar gemacht werden können.
 				</p>
 				<p>Vor diesem Hintergrund wurden prosopographische, universitätsgeschichtliche bzw. gelehrtengeschichtliche Daten den FAIR-Data-Prinzipien gemäß nach Semantic-Web-Standards modelliert und eine Domänenontolo­gie entworfen, welche die Spezifik des Gelehrten Feldes der Frühen Neuzeit im deutschsprachigen Raum re­flektiert und das vorhanden Datenmaterial forschungsspezifisch arrangiert. Die aktuelle Version 0.2 beinhal­tet das Modell mit relevanten Konzepten und deren Relationen.
 				</p>
+				<p>Im Rahmen Abschlusskonferenz am 20. und 21. März 2023 werden sich die Partner und assoziierte Partner im Projekt treffen um die Ergebnisse des Projekte intensiv zu diskutieren. Es sind die Teilnehmer des pandemiebedingt im Oktober 2021 online stattgefundenen Workshops ebenso eingeladen, wie die Projektgruppe von Historikern aus der dem Verbundprojekt "Vernetzte Pfarrerbücher", welche im Rahmen eines Modellierungsworkshops die Projektergebnisse anwenden werden.
+				</p>
+			<p>Die Dokumentation dieser Beiträge erfolgte in Form eines Bandes in der Reihe Beiträge zur Informatik beim Open-Access-Verlag der HTWK Leipzig und wird  präsentiert.</p>
+					
+
+
 			</div>
 
 				
