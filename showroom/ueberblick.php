d99d1953dd59d8984d458eca6cb4551347ce6247
T. Riechert <thomas.riechert@htwk-leipzig.de>
2023-03-30 18:45:21 +0200

    Überblick

--- showroom/ueberblick.php
+++ showroom/ueberblick.php
@@ -1,18 +1,19 @@
 	<div class="row">
 
 		<div class="col-12">
-			<h3>Thema</h3>
+			<h3>Projektergebnisse</h3>
 
 			<div class="text">
-				<p>Das DFG-Projekt „Professorale Karrieremuster der Frühen Neuzeit: Entwicklung einer wissenschaftlichen Methode zur Forschung auf online verfügbaren und verteilten Forschungsdatenbanken der Universitätsge­schichte“ (2016-2023), durchgeführt von der Herzog August Bibliothek Wolfenbüttel in Kooperation mit der Hochschule für Technik, Wirtschaft und Kultur in Leipzig, lädt zur Abschlusskonferenz ein.
+				<p>Die folgenden Seiten demonstrieren einen Teil der Projektergebnisse im Rahmen des DFG-Projektes „Professorale Karrieremuster der Frühen Neuzeit: Entwicklung einer wissenschaftlichen Methode zur Forschung auf online verfügbaren und verteilten Forschungsdatenbanken der Universitätsge­schichte“ (2016-2023).
 				</p>
-				<p>Leitfrage des Projekts war die nach den Bedingungen für den beruflichen Erfolg von Professoren im frühneu­zeitlichen Universitätssystem. Dabei war es das Anliegen zu erproben wie mögliche gelehrte Karrieremuster mit den Methoden der Digital Humanities sichtbar gemacht werden können.
+				<p>Wir möchten Sie mit diesem Demonstrator dazu einladen, die entstandene Forschungsontologie zu erkunden. Dafür stellen wir Ihnen 3 Werkzeuge zur Verfügung.
 				</p>
-				<p>Vor diesem Hintergrund wurden prosopographische, universitätsgeschichtliche bzw. gelehrtengeschichtliche Daten den FAIR-Data-Prinzipien gemäß nach Semantic-Web-Standards modelliert und eine Domänenontolo­gie entworfen, welche die Spezifik des Gelehrten Feldes der Frühen Neuzeit im deutschsprachigen Raum re­flektiert und das vorhanden Datenmaterial forschungsspezifisch arrangiert. Die aktuelle Version 0.2 beinhal­tet das Modell mit relevanten Konzepten und deren Relationen.
+				<p>Der <b>OntoExplorer</b> wurde entwickelt um generisch RDF-Wissensdatenbanken, welche per SPARQL-Endpunkt im Netz verfügbar sind, zu erbrowsen. Das Werkzeug ist Open Source und basiert auf dem JavaScript-Framework React. 
 				</p>
-				<p>Im Rahmen Abschlusskonferenz am 20. und 21. März 2023 werden sich die Partner und assoziierte Partner im Projekt treffen um die Ergebnisse des Projekte intensiv zu diskutieren. Es sind die Teilnehmer des pandemiebedingt im Oktober 2021 online stattgefundenen Workshops ebenso eingeladen, wie die Projektgruppe von Historikern aus der dem Verbundprojekt "Vernetzte Pfarrerbücher", welche im Rahmen eines Modellierungsworkshops die Projektergebnisse anwenden werden.
+				<p>Das <b>Vokabular</b> zur Beschreibung der Forschungsdaten ist eine einer Dokumentation verfügbar.
 				</p>
-			<p>Die Dokumentation dieser Beiträge erfolgte in Form eines Bandes in der Reihe Beiträge zur Informatik beim Open-Access-Verlag der HTWK Leipzig und wird  präsentiert.</p>
+			<p>Neben dem einfachen Browsen der Daten, können per Abfragesprache SPARQL auch <b>komplexe Anfragen</b> an die Forschungsontologie gestellt werden. 
+			</p>
 					
 
 
